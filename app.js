26728cfbe00efcfaa2e72aadf66f1dc674ef1c9d
yasharalee <77319904+yasharalee@users.noreply.github.com>
2023-07-24 22:32:45 -0700

    cors

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(
   cors({
     origin: [
       "https://98.246.0.185",
-      "https://localhost:3000",
+      //"https://localhost:3000",
       "https://selinaystore.com",
     ],
     credentials: true, 
