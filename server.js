8351064ce3755aabbc82b852dad0adf659b69e38
yasharalee <77319904+yasharalee@users.noreply.github.com>
2023-07-27 23:41:31 -0700

    https on local and server

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const mongoose = require("mongoose");
 const app = require("./app");
 
 const PORT = process.env.PORT || 3001;
+const isProduction = process.env.NODE_ENV === 'production';
 
 // Connect to MongoDB
 mongoose
@@ -13,10 +16,25 @@ mongoose
   .then(() => {
     console.log("Connected to MongoDB");
 
-    // Create HTTP server
-    http.createServer(app).listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
+    if (isProduction) {
+      // Create HTTPS server for production
+      const httpsOptions = {
+        key: fs.readFileSync('/etc/letsencrypt/live/lapisel-dev.com/privkey.pem'),
+        cert: fs.readFileSync('/etc/letsencrypt/live/lapisel-dev.com/fullchain.pem')
+      };
+      https.createServer(httpsOptions, app).listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+      });
+    } else {
+      // Create HTTPS server for local development using self-signed certificates
+      const httpsOptions = {
+        key: fs.readFileSync('path/to/private-key.pem'),
+        cert: fs.readFileSync('path/to/certificate.pem')
+      };
+      https.createServer(httpsOptions, app).listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+      });
+    }
   })
   .catch((err) => {
     console.error("Error connecting to MongoDB:", err);
