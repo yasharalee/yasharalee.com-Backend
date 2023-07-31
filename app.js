6803a0be2455dd0fa70d02067c8938e544316e26
yasharalee <77319904+yasharalee@users.noreply.github.com>
2023-07-31 15:59:02 -0700

    l

--- app.js
+++ app.js
@@ -35,16 +35,7 @@ const allowedOrigins = [
 
 // CORS middleware
 app.use(
-  cors({
-    origin: function (origin, callback) {
-      
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+  cors({origin: "*",
     credentials: true,
     allowedHeaders: "content-type",
   })
