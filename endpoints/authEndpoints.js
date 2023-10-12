905ca423018f6f3bcb16b9aef0380a1661342a25
yasharalee <77319904+yasharalee@users.noreply.github.com>
2023-10-11 22:09:43 -0700

    redirect

--- endpoints/authEndpoints.js
+++ endpoints/authEndpoints.js
@@ -27,7 +27,7 @@ const googleCallback = (req, res, next) => {
 
 
         jwtCookie.setHttpOnlyCookie(res, "access-token", token, new Date(Date.now() + 1 * 60 * 60 * 1000),"/")
-        res.redirect('https://localhost/test'); 
+        res.redirect('https://yaslanding.com/test'); 
     })(req, res, next);
 };
 
